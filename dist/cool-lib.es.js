04fdd5e9eda18027992caea065ac05fef25b37e3
Alioth <z610886764@163.com>
2021-08-05 13:58:24 +0800

    feat: add fileToDataURL

--- dist/cool-lib.es.js
+++ dist/cool-lib.es.js
@@ -1,64 +1,62 @@
 // 调用
 // 利用new File()实现，不兼容ios11.4以下
 // var file = dataURLtoFile(base64Data, imgName);
-
 // 将base64转成blob ——> blob转成file，无兼容性问题
 // var blob = dataURLtoBlob(base64Data);
 // var file = blobToFile(blob, imgName);
-
 // 将文件转base64
-export function fileToDataURL(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.addEventListener(
-      "load",
-      function () {
-        resolve(reader.result);
-      },
-      false
-    );
+function fileToDataURL(file) {
+  return new Promise(function (resolve, reject) {
+    var reader = new FileReader();
+    reader.addEventListener("load", function () {
+      resolve(reader.result);
+    }, false);
+
     if (file) {
       reader.readAsDataURL(file);
     } else {
       resolve(null);
     }
   });
-}
+} // 将base64转换为文件
 
-// 将base64转换为文件
-export function dataURLtoFile(dataURL, filename) {
+function dataURLtoFile(dataURL, filename) {
   var arr = dataURL.split(","),
-    mime = arr[0].match(/:(.*?);/)[1],
-    bstr = atob(arr[1]),
-    n = bstr.length,
-    u8arr = new Uint8Array(n);
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+
   while (n--) {
     u8arr[n] = bstr.charCodeAt(n);
   }
-  return new File([u8arr], filename, { type: mime });
-}
 
-// 将base64转换为blob
-export function dataURLtoBlob(dataURL) {
+  return new File([u8arr], filename, {
+    type: mime
+  });
+} // 将base64转换为blob
+
+function dataURLtoBlob(dataURL) {
   var arr = dataURL.split(","),
-    mime = arr[0].match(/:(.*?);/)[1],
-    bstr = atob(arr[1]),
-    n = bstr.length,
-    u8arr = new Uint8Array(n);
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+
   while (n--) {
     u8arr[n] = bstr.charCodeAt(n);
   }
-  return new Blob([u8arr], { type: mime });
-}
 
-// 将blob转换为file
-export function blobToFile(theBlob, fileName) {
+  return new Blob([u8arr], {
+    type: mime
+  });
+} // 将blob转换为file
+
+function blobToFile(theBlob, fileName) {
   theBlob.lastModifiedDate = new Date();
   theBlob.name = fileName;
   return theBlob;
-}
-
-// 前端图片压缩库源自：https://github.com/shb190802/picture-compressor
+} // 前端图片压缩库源自：https://github.com/shb190802/picture-compressor
 
 /**
  * 将图片压缩为对应尺寸
@@ -72,66 +70,62 @@ export function blobToFile(theBlob, fileName) {
  * @param {Number} options.rotate 图片旋转，由于手机拍照的角度和我们使用的头像不一致，需要旋转 默认0 仅支持 90 180 -90
  * @returns {Promise} then {width,height,img}
  */
-export function pictureCompress(options) {
-  return new Promise((resolve, reject) => {
+
+function pictureCompress(options) {
+  return new Promise(function (resolve, reject) {
     if (!options.img) {
       reject(new Error("need img"));
       return;
     }
-    let imgSrc = options.img,
-      width = options.width || 640,
-      height = options.height || 640,
-      type = options.type || "jpg",
-      quality = options.quality || 0.92,
-      fit = options.fit || "scale",
-      rotate = options.rotate || 0;
+
+    var imgSrc = options.img,
+        width = options.width || 640,
+        height = options.height || 640,
+        type = options.type || "jpg",
+        quality = options.quality || 0.92,
+        fit = options.fit || "scale",
+        rotate = options.rotate || 0;
+
     if (width <= 0 || height <= 0) {
       reject(new Error("dist width or height need > 0"));
       return;
     }
+
     if (!/jpg|png|jpeg/.test(type)) {
       reject(new Error("type need jpg or png!"));
       return;
     }
+
     if (rotate !== 90 && rotate !== -90 && rotate !== 0 && rotate !== 180) {
       reject(new Error("rotate mast be 0 90 -90 180!"));
       return;
     }
-    let changeWidthAndHeight = rotate === 90 || rotate === -90;
-    let image = new Image();
+
+    var changeWidthAndHeight = rotate === 90 || rotate === -90;
+    var image = new Image();
     image.src = imgSrc;
+
     image.onload = function () {
-      let distSize = getDistSize(
-        {
-          width: changeWidthAndHeight ? this.naturalHeight : this.naturalWidth,
-          height: changeWidthAndHeight ? this.naturalWidth : this.naturalHeight,
-        },
-        {
-          width: changeWidthAndHeight ? height : width,
-          height: changeWidthAndHeight ? width : height,
-        },
-        fit
-      );
-      let imgData = compress(
-        this,
-        distSize.width,
-        distSize.height,
-        type,
-        quality,
-        rotate
-      );
+      var distSize = getDistSize({
+        width: changeWidthAndHeight ? this.naturalHeight : this.naturalWidth,
+        height: changeWidthAndHeight ? this.naturalWidth : this.naturalHeight
+      }, {
+        width: changeWidthAndHeight ? height : width,
+        height: changeWidthAndHeight ? width : height
+      }, fit);
+      var imgData = compress(this, distSize.width, distSize.height, type, quality, rotate);
       resolve({
         width: distSize.width,
         height: distSize.height,
-        img: imgData,
+        img: imgData
       });
     };
+
     image.onerror = function (err) {
       reject(err);
     };
   });
 }
-
 /**
  * 将图片转换为固定尺寸的
  * @param {Image} img 图片数据
@@ -140,49 +134,60 @@ export function pictureCompress(options) {
  * @param {String} type base64的图片类型 jpg png
  * @param {Number} quality 转换之后的图片质量
  */
+
 function compress(img, width, height, type, quality, rotate) {
   var canvas = document.createElement("canvas");
   var ctx = canvas.getContext("2d");
-  let types = {
+  var types = {
     jpg: "image/jpeg",
     jpeg: "image/jpeg",
-    png: "image/png",
+    png: "image/png"
   };
   canvas.width = width;
   canvas.height = height;
+
   if (rotate === 90) {
     ctx.translate(width, 0);
-    ctx.rotate((90 * Math.PI) / 180);
+    ctx.rotate(90 * Math.PI / 180);
     ctx.drawImage(img, 0, 0, height, width);
   } else if (rotate === -90) {
     ctx.translate(0, height);
-    ctx.rotate((-90 * Math.PI) / 180);
+    ctx.rotate(-90 * Math.PI / 180);
     ctx.drawImage(img, 0, 0, height, width);
   } else if (rotate === 180) {
     ctx.translate(width, height);
-    ctx.rotate((180 * Math.PI) / 180);
+    ctx.rotate(180 * Math.PI / 180);
     ctx.drawImage(img, 0, 0, width, height);
   } else {
     ctx.drawImage(img, 0, 0, width, height);
   }
+
   return canvas.toDataURL(types[type], quality);
 }
-
 /**
  * 选择源尺寸与目标尺寸比例中较小的那个，保证图片可以完全显示
  * 最大值不超过1，如果图片源尺寸小于目标尺寸，则不做处理，返回图片原尺寸
  * @param {Object} source 源图片的宽高
  * @param {Object} dist 目标图片的宽高
  */
+
+
 function getDistSize(source, dist, fit) {
   if (fit === "fill") return dist;
-  let scale = Math.min(
-    dist.width / source.width,
-    dist.height / source.height,
-    1
-  );
+  var scale = Math.min(dist.width / source.width, dist.height / source.height, 1);
   return {
     width: Math.round(source.width * scale),
-    height: Math.round(source.height * scale),
+    height: Math.round(source.height * scale)
   };
 }
+
+var mediaPicture = /*#__PURE__*/Object.freeze({
+  __proto__: null,
+  fileToDataURL: fileToDataURL,
+  dataURLtoFile: dataURLtoFile,
+  dataURLtoBlob: dataURLtoBlob,
+  blobToFile: blobToFile,
+  pictureCompress: pictureCompress
+});
+
+export { mediaPicture };
